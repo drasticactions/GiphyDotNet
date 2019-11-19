bc0b5551cb9afb49744f53014ed373d0a55bdd4d
Tim Miller <innerlogic4321@gmail.com>
2019-11-18 21:27:48 -0500

    Revamp Library
    - Move to .NET Standard 2.0
    - Add Unit Tests

---
+++ GiphyDotNet.Test/BasicTest.cs
@@ -0,0 +1,79 @@
+using GiphyDotNet.Manager;
+using GiphyDotNet.Model.Parameters;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace GiphyDotNet.Test
+{
+    public class BasicTest
+    {
+        string api_token;
+        Giphy giphy;
+        public BasicTest()
+        {
+            api_token = Environment.GetEnvironmentVariable("GIPHY_TOKEN");
+            if (string.IsNullOrEmpty(api_token))
+                throw new Exception("You must set the API Token in your Environment Variables!");
+            giphy = new Giphy(api_token);
+        }
+
+        [Fact]
+        public async void GifSearch()
+        {
+            var searchParameter = new SearchParameter()
+            {
+                Query = "awesome"
+            };
+
+            // Returns gif results
+            var gifResult = await giphy.GifSearch(searchParameter);
+            Assert.True(gifResult != null);
+            Assert.True(gifResult.Data.Any());
+        }
+
+        [Fact]
+        public async void StickerSearch()
+        {
+            var searchParameter = new SearchParameter()
+            {
+                Query = "awesome"
+            };
+
+            // Returns gif results
+            var gifResult = await giphy.StickerSearch(searchParameter);
+            Assert.True(gifResult != null);
+            Assert.True(gifResult.Data.Any());
+        }
+
+        [Fact]
+        public async void IdSearch()
+        {
+            var gifResult = await giphy.GetGifById("feqkVgjJpYtjy");
+            Assert.True(gifResult != null);
+            var gifsResult = await giphy.GetGifsByIds(new string[] { "feqkVgjJpYtjy", "7rzbxdu0ZEXLy" });
+            Assert.True(gifsResult != null);
+            Assert.True(gifsResult.Data.Length > 0);
+        }
+
+        [Fact]
+        public async void RandomGifSearch()
+        {
+            var gifResult = await giphy.RandomGif(new RandomParameter()
+            {
+                Tag = "american psycho"
+            });
+            Assert.True(gifResult != null);
+        }
+
+        [Fact]
+        public async void RandomStickerSearch()
+        {
+            var gifResult = await giphy.RandomSticker(new RandomParameter()
+            {
+                Tag = "american psycho"
+            });
+            Assert.True(gifResult != null);
+        }
+    }
+}
