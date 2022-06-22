07974bcaf59e722a42c5f4c3c32744fe987c641e
Tim Miller <innerlogic4321@gmail.com>
2022-06-22 19:03:23 -0400

    Fix Random Search

--- GiphyDotNet/Model/GiphyRandomImage/Data.cs
+++ GiphyDotNet/Model/GiphyRandomImage/Data.cs
@@ -3,6 +3,7 @@
 // </copyright>
 
 using System.Text.Json.Serialization;
+using GiphyDotNet.Model.GiphyImage;
 
 namespace GiphyDotNet.Model.GiphyRandomImage
 {
@@ -17,64 +18,52 @@ namespace GiphyDotNet.Model.GiphyRandomImage
         [JsonPropertyName("url")]
         public string? Url { get; set; }
 
-        [JsonPropertyName("image_original_url")]
-        public string? ImageOriginalUrl { get; set; }
+        [JsonPropertyName("slug")]
+        public string? Slug { get; set; }
 
-        [JsonPropertyName("image_url")]
-        public string? ImageUrl { get; set; }
+        [JsonPropertyName("bitly_gif_url")]
+        public string? BitlyGifUrl { get; set; }
 
-        [JsonPropertyName("image_mp4_url")]
-        public string? ImageMp4Url { get; set; }
+        [JsonPropertyName("bitly_url")]
+        public string? BitlyUrl { get; set; }
 
-        [JsonPropertyName("image_frames")]
-        public string? ImageFrames { get; set; }
+        [JsonPropertyName("embed_url")]
+        public string? EmbedUrl { get; set; }
 
-        [JsonPropertyName("image_width")]
-        public string? ImageWidth { get; set; }
+        [JsonPropertyName("username")]
+        public string? Username { get; set; }
 
-        [JsonPropertyName("image_height")]
-        public string? ImageHeight { get; set; }
+        [JsonPropertyName("source")]
+        public string? Source { get; set; }
 
-        [JsonPropertyName("fixed_height_downsampled_url")]
-        public string? FixedHeightDownsampledUrl { get; set; }
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
 
-        [JsonPropertyName("fixed_height_downsampled_width")]
-        public string? FixedHeightDownsampledWidth { get; set; }
+        [JsonPropertyName("rating")]
+        public string? Rating { get; set; }
 
-        [JsonPropertyName("fixed_height_downsampled_height")]
-        public string? FixedHeightDownsampledHeight { get; set; }
+        [JsonPropertyName("content_url")]
+        public string? ContentUrl { get; set; }
 
-        [JsonPropertyName("fixed_width_downsampled_url")]
-        public string? FixedWidthDownsampledUrl { get; set; }
+        [JsonPropertyName("source_tld")]
+        public string? SourceTld { get; set; }
 
-        [JsonPropertyName("fixed_width_downsampled_width")]
-        public string? FixedWidthDownsampledWidth { get; set; }
+        [JsonPropertyName("source_post_url")]
+        public string? SourcePostUrl { get; set; }
 
-        [JsonPropertyName("fixed_width_downsampled_height")]
-        public string? FixedWidthDownsampledHeight { get; set; }
+        [JsonPropertyName("is_sticker")]
+        public int IsSticker { get; set; }
 
-        [JsonPropertyName("fixed_height_small_url")]
-        public string? FixedHeightSmallUrl { get; set; }
+        [JsonPropertyName("import_datetime")]
+        public string? ImportDatetime { get; set; }
 
-        [JsonPropertyName("fixed_height_small_still_url")]
-        public string? FixedHeightSmallStillUrl { get; set; }
+        [JsonPropertyName("trending_datetime")]
+        public string? TrendingDatetime { get; set; }
 
-        [JsonPropertyName("fixed_height_small_width")]
-        public string? FixedHeightSmallWidth { get; set; }
+        [JsonPropertyName("images")]
+        public Images? Images { get; set; }
 
-        [JsonPropertyName("fixed_height_small_height")]
-        public string? FixedHeightSmallHeight { get; set; }
-
-        [JsonPropertyName("fixed_width_small_url")]
-        public string? FixedWidthSmallUrl { get; set; }
-
-        [JsonPropertyName("fixed_width_small_still_url")]
-        public string? FixedWidthSmallStillUrl { get; set; }
-
-        [JsonPropertyName("fixed_width_small_width")]
-        public string? FixedWidthSmallWidth { get; set; }
-
-        [JsonPropertyName("fixed_width_small_height")]
-        public string? FixedWidthSmallHeight { get; set; }
+        //[JsonPropertyName("user")]
+        //public User? User { get; set; }
     }
 }
